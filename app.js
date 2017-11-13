4deace1db786efcb64c4d13c4d915a4159545379
Patrick Hill <patrickrhill@gmail.com>
2017-11-13 12:34:35 -0500

    adds js for scroll listening

---
+++ app.js
@@ -0,0 +1,41 @@
+
+let latestKnownScrollY = 0;
+let ticking = false;
+let windowHeight = window.innerHeight;
+const animatedEl = document.getElementsByClassName("animate-onscroll");
+
+function update() {
+	ticking = false;
+  const activationPoint = latestKnownScrollY + (windowHeight * 0.5);
+
+  for (let i = 0; i < animatedEl.length; i++) {
+    // set base animation styles in css with .can-animate-onscroll
+    // so browsers that don't support RAF won't show unfinished animation states
+    animatedEl[i].classList.add("can-animate-onscroll");
+    if (animatedEl[i].offsetTop < (activationPoint) ){
+      animatedEl[i].classList.add("animation-start");
+    } else {
+      animatedEl[i].classList.remove("animation-start");
+    }
+  }
+
+}
+
+function requestTick() {
+	if(!ticking) {
+		requestAnimationFrame(update);
+	}
+	ticking = true;
+}
+
+function onScroll() {
+	latestKnownScrollY = window.scrollY;
+	requestTick();
+}
+function onResize() {
+	windowHeight = window.innerHeight;
+}
+
+window.addEventListener('scroll', onScroll, false);
+window.addEventListener('resize', onResize, false);
+
