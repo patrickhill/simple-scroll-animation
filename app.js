1cada304be04bf6438d29fcb65d07ef500c02a73
Patrick Hill <patrickrhill@gmail.com>
2019-05-16 13:32:53 -0400

    adds paused class

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
 let ticking = false;
-let activationPoint = (window.innerHeight * 0.5);
-const animatedElems = document.getElementsByClassName("animate-onscroll");
+let activationPoint = window.innerHeight * 0.5;
+const animatedElems = document.getElementsByClassName('animate-onscroll');
 
 function initAnimations() {
   for (let i = 0; i < animatedElems.length; i++) {
     // add can-animate so browsers that don't support rAF won't show unfinished animation states
-    animatedElems[i].classList.add("can-animate-onscroll");
+    animatedElems[i].classList.add('can-animate-onscroll');
   }
 }
 function update() {
   for (let i = 0; i < animatedElems.length; i++) {
     const eleY = animatedElems[i].getBoundingClientRect().y;
     const elemClass = animatedElems[i].classList;
-    if ( eleY < activationPoint ){
-      elemClass.add("animate-onscroll-start");
-    } else if ( elemClass.contains("animate-onscroll-start") ) {
-      elemClass.remove("animate-onscroll-start");
+    if (eleY < activationPoint) {
+      elemClass.add('animate-onscroll-running');
+      elemClass.remove('animate-onscroll-paused');
+    } else if (elemClass && elemClass.contains('animate-onscroll-running')) {
+      elemClass.remove('animate-onscroll-running');
+      elemClass.add('animate-onscroll-paused');
     }
   }
-  // when finished with stuff, reset ticking so new rAF can be started
   ticking = false;
 }
 
 function onScroll() {
-  // check if rAF is already running
-  if(!ticking) {
-		requestAnimationFrame(update);
-    // set rAF as running so we don't call it again until last one finishes
+  if (!ticking) {
+    requestAnimationFrame(update);
     ticking = true;
-	}
+  }
 }
 function onResize() {
-  activationPoint = (window.innerHeight * 0.5);
+  activationPoint = window.innerHeight * 0.5;
 }
 
 if (animatedElems.length > 0) {
